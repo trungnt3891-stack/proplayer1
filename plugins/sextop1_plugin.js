292bcca2889e24f25fbc362b6ec87fc2b04ce326
youngbi <32502366+youngbi@users.noreply.github.com>
2026-07-25 21:26:48 +0700

    Change playerType from 'embedtoexoplay' to 'embed'

--- plugins/sextop1_plugin.js
+++ plugins/sextop1_plugin.js
@@ -12,7 +12,7 @@ function getManifest() {
         "isEnabled": true,
         "isAdult": true,
         "type": "VIDEO",
-        "playerType": "embedtoexoplay",
+        "playerType": "embed",
         "layoutType": "HORIZONTAL"
     });
 }
