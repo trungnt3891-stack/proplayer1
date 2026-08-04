a08db9b9344d083246a104d76af98912240df3db
youngbi <32502366+youngbi@users.noreply.github.com>
2026-08-05 00:57:36 +0700

    Update 123av_plugin.js

--- plugins/123av_plugin.js
+++ plugins/123av_plugin.js
@@ -6,13 +6,13 @@ function getManifest() {
     return JSON.stringify({
         "id": "123av",
         "name": "123AV",
-        "version": "1.0.4",
+        "version": "1.0.5",
         "baseUrl": "https://123av.com",
         "referrer": "https://123av.com/",
         "iconUrl": "https://123av.com/assets/123av/favicon.png",
         "isEnabled": true,
         "isAdult": true,
-        "type": "VIDEO",
+        "type": "MOVIE",
         "layoutType": "HORIZONTAL",
         "subtitleCat": false
     });
